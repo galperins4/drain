b3a5b488a8349c6c5b64b63c31d75b149998079a
galperins4 <galperins4@gmail.com>
2025-03-22 17:33:53 -0500

    Update exchange.py

--- modules/exchange.py
+++ modules/exchange.py
@@ -22,6 +22,8 @@ class Exchange:
             pay = self.process_simpleswap_exchange(address, amount)
         elif provider == "StealthEx":
             pay = self.process_stealth_exchange(address, amount)
+        elif provider == "Swapzone":
+            pay = self.process_swapzone_exchange(address, amount)
         else:
             pay = address
         time.sleep(5)
@@ -136,5 +138,43 @@ class Exchange:
             payin_address = address
             self.logger.error("Exchange Fail")
     
+        self.logger.info(f"Pay In Address {payin_address}")
+        return payin_address
+
+    
+    def process_swapzone_exchange(self, address, amount):
+        self.logger.info("Processing Exchange")
+        amount = self.truncate((amount / self.config['atomic']),4)
+        self.logger.info(f"Exchange Amount: {amount}")
+        url = 'https://4sp4w95r1i.execute-api.us-west-2.amazonaws.com/default/exchange'
+        data_in = {"from": self.config['convert_from'],
+                   "to": self.config['convert_to'],
+                   "addressReceive": self.config['address_to'],
+                   "amountDeposit": str(amount),
+                   "refundAddress":address}
+        res_bytes={}
+        res_bytes['data'] = json.dumps(data_in).encode('utf-8')
+
+        try:
+            r = requests.get(url, params=res_bytes)
+            print(r.json())
+            if r.json()['status'] == "success":
+                payin_address = r.json()['payinAddress']
+                exchangeid = r.json()['exchangeId']
+                now = datetime.now()
+                output = [payin_address, exchangeid, now.isoformat()]
+                f_out = ','.join(output)
+                file = open('exchange.txt','a')
+                f_out = ','.join(output)
+                file.write(f_out+"\n")
+                file.close()
+                self.logger.info("Exchange Success") 
+            else:
+                payin_address = address
+                self.logger.error("Exchange Fail")
+        except:
+            payin_address = address
+            self.logger.error("Exchange Fail")
+ 
         self.logger.info(f"Pay In Address {payin_address}")
         return payin_address
